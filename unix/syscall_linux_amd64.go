b9a3733514ced39880a317e8d347bd2475c2c7aa
Connor McEwen <connor.mcewen@gmail.com>
2020-08-16 15:41:01 -0700

    epoll_wait to epoll_pwait

--- unix/syscall_linux_amd64.go
+++ unix/syscall_linux_amd64.go
@@ -8,7 +8,7 @@ package unix
 
 //sys	dup2(oldfd int, newfd int) (err error)
 //sysnb	EpollCreate(size int) (fd int, err error)
-//sys	EpollWait(epfd int, events []EpollEvent, msec int) (n int, err error)
+//sys	EpollWait(epfd int, events []EpollEvent, msec int) (n int, err error) = SYS_EPOLL_PWAIT
 //sys	Fadvise(fd int, offset int64, length int64, advice int) (err error) = SYS_FADVISE64
 //sys	Fchown(fd int, uid int, gid int) (err error)
 //sys	Fstat(fd int, stat *Stat_t) (err error)
